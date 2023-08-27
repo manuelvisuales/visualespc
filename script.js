5619156da08170e847a6f3ce5ed3cc29b9873aa7
Manuel <120177542+manuelvisuales@users.noreply.github.com>
2023-08-27 08:13:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const gpt3 = new OpenAI({
+    apiKey: 'TU_CLAVE_DE_API' // Reemplaza con tu clave de API de OpenAI
+  });
+  
+  async function realizarSuma() {
+    const numero1 = parseFloat(document.getElementById('numero1').value);
+    const numero2 = parseFloat(document.getElementById('numero2').value);
+  
+    if (!isNaN(numero1) && !isNaN(numero2)) {
+      const respuesta = await gpt3.answer({
+        question: `Realiza la suma ${numero1} + ${numero2}.`,
+        examplesContext: 'Realiza la siguiente operación matemática:',
+        documents: [],
+        max_tokens: 30
+      });
+  
+      document.getElementById('resultado').textContent = `La suma es: ${respuesta}`;
+    } else {
+      document.getElementById('resultado').textContent = 'Ingresa números válidos.';
+    }
+  }
